b0580b859311415dd9a058f8f2e806a431683365
fl1jason <jason@fl1digital.com>
2023-04-12 17:31:47 +0100

    changed endpoint for WooCommerce

--- src/env.tsx
+++ src/env.tsx
@@ -1,11 +1,11 @@
 const config = {
-			version: 0,
-			wp_endpoint: "https://fl1digital.com/wp-json/wp/v2/",
-			woo_endpoint: "https://dev.thepriceiswight.co.uk/wp-json/wc/v2/",
-			woo_key:
-				"ck_47c11621051625a03e51c24d4770fe1539fee7eb",
-				woo_secret:
-				"cs_376e156d8a5454df4c936246b69b2016dafdc0fb",
-	  };
+	version: 0,
+	wp_endpoint: "https://fl1digital.com/wp-json/wp/v2/",
+	woo_endpoint: "https://fl1digital.com/wp-json/wc/v2/",
+	woo_key:
+		"ck_47c11621051625a03e51c24d4770fe1539fee7eb",
+	woo_secret:
+		"cs_376e156d8a5454df4c936246b69b2016dafdc0fb",
+};
 
 export default config;
